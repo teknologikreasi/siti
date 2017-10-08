45d720cb3c5e4fa8761e379fb7c5c082be770933
Unknown <yolandahertita903@gmail.com>
2017-10-08 12:32:15 +0700

    tiutut

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lita
                 </div>
 
                 <div class="links">
